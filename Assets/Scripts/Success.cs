431ac5ded5972778fc5c644a9dbb5f1875570713
JacksonPSnyder <jps6302@uncw.edu>
2025-04-28 12:04:22 -0400

    update

--- Assets/Scripts/Success.cs
+++ Assets/Scripts/Success.cs
@@ -4,11 +4,11 @@ using UnityEngine.SceneManagement;
 
 public class FadeAndEndScene : MonoBehaviour
 {
-    public GameObject mask;                 // Assign your mask object here
+    public GameObject mask;                 
     public Vector2 targetCenter;             // The center of the target range
     public Vector2 targetSize = new Vector2(1f, 1f); // Size of the target range (width, height)
 
-    public Image fadeImage;                  // A UI Image (full screen, black, with alpha 0 initially)
+    public Image fadeImage;                  
     public float fadeSpeed = 1f;              // How fast to fade to black
 
     private bool shouldFade = false;
@@ -51,12 +51,10 @@ public class FadeAndEndScene : MonoBehaviour
 
     void EndScene()
     {
-        // You can load another scene or quit
-        // Example: Load the next scene in the build index
+        
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 
-        // Or just quit the application if needed
-        // Application.Quit();
+        
     }
 }
 
